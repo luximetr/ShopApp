7312f1d25c9fdc5daa8f1182efb7fbdb84184fd7
Oleksandr Orlov <luximetr@gmail.com>
2020-07-25 14:01:10 +0800

    Initial Commit

---
+++ ShopApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShopApp
+//
+//  Created by Oleksandr Orlov on 25/7/20.
+//  Copyright © 2020 Deskera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
